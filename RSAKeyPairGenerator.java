475fd2ad59d0c691e598c78b55c882b8b20f551c
Rugie Gabitanan <58616578+RugieGabitanan@users.noreply.github.com>
2024-05-25 16:06:41 +0800

    add key generator

--- RSAKeyPairGenerator.java
+++ RSAKeyPairGenerator.java
@@ -1,3 +1,17 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 public class RSAKeyPairGenerator {
-    
+    public static void main(String[] args) throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048); // Key size
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+        
+        System.out.println("Public Key: " + publicKey);
+        System.out.println("Private Key: " + privateKey);
+    }    
 }
